7888656831f1164ba532a239ef4cef898e3ad6c0
kimknightrichard0917 <kimknightrichard0917@outlook.com>
2026-07-03 12:45:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
-# Configuration: Change these or set as Environment Variables in Railway
-USER_NAME=${USER_NAME:-"admin"}
-USER_PASSWORD=${USER_PASSWORD:-"railway123"}
+# Set Root Password
+echo "root:root" | chpasswd
 
-# 1. Create User
-if ! id "$USER_NAME" &>/dev/null; then
-    useradd -m -s /bin/bash "$USER_NAME"
-    echo "$USER_NAME:$USER_PASSWORD" | chpasswd
-    echo "$USER_NAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-fi
-
-# 2. Fix Railway Shared Memory & Cleanup
+# 1. Fix Permissions & Shared Memory
 rm -rf /var/run/xrdp/*
 mkdir -p /var/run/xrdp
 chown xrdp:xrdp /var/run/xrdp
 
-# 3. Optimization: Disable XFCE heavy graphics for the user
-sudo -u "$USER_NAME" mkdir -p /home/"$USER_NAME"/.config/xfce4/xfconf/xfce-perchannel-xml
-cat <<EOF > /home/"$USER_NAME"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+# 2. Create Desktop Shortcuts for Browsers in Root folder
+mkdir -p /root/Desktop
+
+# Firefox Shortcut
+cat <<EOF > /root/Desktop/firefox.desktop
+[Desktop Entry]
+Name=Firefox
+Exec=firefox-esr
+Icon=firefox-esr
+Type=Application
+EOF
+
+# Chrome Shortcut
+cat <<EOF > /root/Desktop/chrome.desktop
+[Desktop Entry]
+Name=Chrome
+Exec=/usr/local/bin/chrome
+Icon=chromium
+Type=Application
+EOF
+
+chmod +x /root/Desktop/*.desktop
+
+# 3. Optimization: Disable XFCE heavy graphics for Root
+mkdir -p /root/.config/xfce4/xfconf/xfce-perchannel-xml
+cat <<EOF > /root/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <channel name="xfwm4" version="1.0">
   <property name="general" type="empty">
@@ -27,6 +42,6 @@ cat <<EOF > /home/"$USER_NAME"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xm
 </channel>
 EOF
 
-# 4. Start Services
+# 4. Start XRDP Services
 /usr/sbin/xrdp-sesman --nodaemon &
 /usr/sbin/xrdp --nodaemon
